cfc908ff31e543bbe2187b6f554b46dce42c7611
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2021-02-16 15:05:03 -0300

    live coding challenge

---
+++ livecoding.py
@@ -0,0 +1,28 @@
+from stores import stores
+
+# You must build the following response, as a Python dictionary
+response_example = {
+    'category_list': [
+        {
+            'category_id': 1,
+            'category_verbose': 'Conveniência',
+        },
+        {
+            'category_id': 1,
+            'category_verbose': 'Conveniência',
+        },
+    ],
+    'stores': stores
+}
+
+# store_stype is a list of all the unique store types that exist in stores
+
+# You task is to implement the function build_response(stores) and return a
+# python dictionary like the response_example defined above. The stores object
+# CAN be mutated.
+
+def build_response(stores):
+    print(stores)
+
+resposne = build_response(stores)
+print(resposne)
