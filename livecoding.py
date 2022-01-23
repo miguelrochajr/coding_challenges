cfb68b6df44682ca75dbf59767d57861432b3ae9
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2022-01-23 18:37:30 -0300

    Update livecoding.py

--- livecoding.py
+++ livecoding.py
@@ -15,13 +15,15 @@ response_example = {
     'stores': stores
 }
 
-# category_list is a list of all the unique store types that exist in stores
+# category_list is a list of all the unique categories that exist in the list stores
 
 # You task is to implement the function build_response(stores) and return a
-# python dictionary like the response_example defined above. The stores object
-# CAN be mutated.
+# python dictionary like the response_example defined above.
 
-def build_response(stores):
+def build_response(stores)
+    # step 1: which categories exist in the list stores?
+    # step 2: build the response.
+    # tip: python dictionary is you friend
     print(stores)
 
 resposne = build_response(stores)
